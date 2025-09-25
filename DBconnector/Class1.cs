64443a3205c2cee509a0066c5f120db2c8fa4209
dylanmxkenzie <dcha4206@students.vsu.edu>
2025-09-24 21:49:14 -0400

    feat add Class1 constructor to initialize MongoDB connection

--- DBconnector/Class1.cs
+++ DBconnector/Class1.cs
@@ -1,7 +1,13 @@
-﻿namespace DBconnector
+﻿using MongoDB.Driver;
+
+namespace DBconnector
 {
     public class Class1
     {
+        private readonly MongoClient _database;
 
+        public DBconnector(string connectionString);
+        {
+        _database = new MongoClient(connectionString);
     }
 }
