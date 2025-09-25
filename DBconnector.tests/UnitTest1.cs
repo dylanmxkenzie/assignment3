a31836e12f0d66fc955481b56af67923cc0468f4
dylanmxkenzie <dcha4206@students.vsu.edu>
2025-09-25 19:31:27 -0400

    finished

--- DBconnector.tests/UnitTest1.cs
+++ DBconnector.tests/UnitTest1.cs
@@ -1,11 +1,36 @@
+using System.Threading.Tasks;
+using Xunit;
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+using DotNet.Testcontainers.Configurations;
+
+
 namespace DBconnector.tests
 {
     public class UnitTest1
     {
         [Fact]
-        public async Task 
+        public void Ping_ShouldReturnTrue_WhenMongoIsReachable()
         {
-            
+            var mongoContainer = new TestcontainersBuilder<MongoDbTestcontainer>()
+                .WithDatabase(new MongoDbTestcontainerConfiguration())
+                .Build();
+
+            mongoContainer.StartAsync().GetAwaiter().GetResult();
+
+            var connector = new Class1(mongoContainer.ConnectionString);
+
+            Assert.True(connector.Ping());
+
+            mongoContainer.DisposeAsync();
+
+        }
+
+        [Fact]
+        public void Ping_ShouldReturnFalse_WhenConnectionFails()
+        {
+            var connector = new Class1("mongodb:// invaild");
+            Assert.False(connector.Ping());
         }
     }
 }
--- DBconnector/Class1.cs
+++ DBconnector/Class1.cs
@@ -1,25 +1,38 @@
 ﻿using MongoDB.Driver;
+using MongoDB.Bson;
+using System.Linq.Expressions;
 
 namespace DBconnector
 {
     public class Class1
     {
-        private readonly MongoClient _database;
+        private readonly IMongoClient _client;
 
-        public DBconnector(string connectionString);
+        public Class1(string connectionString)
         {
-        _database = new MongoClient(connectionString);
-    }
-}
+            _client = new MongoClient(connectionString);
+        }
 
-public bool Ping();
-{
-    return false;
+    public bool Ping()
+        {
+            try
+            {
+                var command = new BsonDocument { { "ping", 1 } };
 
-    try
-    {
-        var db = _database.GetDatabase("admin");
-        var command = new BsonDocument("ping", 1);
-        var result = db.RunCommand<BsonDocument>(command);
+                _client.GetDatabase("admin").RunCommand<BsonDocument>(command);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+                       
+
+
+            }
+        }
     }
-}
+
+
+
