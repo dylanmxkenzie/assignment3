dad8614702e02a3dc9b858a370f9bebaa06a2aa3
dylanmxkenzie <dcha4206@students.vsu.edu>
2025-10-08 20:15:21 -0400

    complete 10/8

--- DBconnector/IDBConnector.cs
+++ DBconnector/IDBConnector.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DBconnector
 {
-    internal interface IDBConnector
+    public interface IDBConnector
     {
         Task<bool> Ping();
     }
---
+++ DBconnector/PostgresConnector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBconnector
+{
+    public class PostgresConnector : IDBConnector
+    {
+        public async Task<bool> Ping()
+        {
+            try
+            {
+                await Task.Delay(10);
+                return true;
+
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
